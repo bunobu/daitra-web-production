54220968874a17239bba75b211635562fb4689cb
bunobu <86172592+bunobu@users.noreply.github.com>
2024-05-13 21:03:35 +0900

    ボタンクリックでアラートの表示追加実装 #5

--- beginner/study/jquery-animation-coding-knock/js/script.js
+++ beginner/study/jquery-animation-coding-knock/js/script.js
@@ -1,6 +1,13 @@
 /**
  * ボタンクリックでアラートの表示
  */
+
+// JSでの実装
+document.getElementById("js-button-alert").addEventListener("click", () => {
+  alert("JS - ボタンがクリックされました！");
+});
+
+// jQueryでの実装
 $("#js-button-alert").on("click", () => {
-  alert("ボタンがクリックされました！");
+  alert("jQuery - ボタンがクリックされました！");
 });
