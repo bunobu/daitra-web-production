541ddc3e98fc9fb9c31598b528dcb6b3f4bbfc04
bunobu <86172592+bunobu@users.noreply.github.com>
2024-05-18 17:55:10 +0900

    TOPへ戻るボタンの実装 #7

--- beginner/projects/jquery-practical-tasks/js/index.js
+++ beginner/projects/jquery-practical-tasks/js/index.js
@@ -1,9 +1,33 @@
-const headerNavMenu = $(".js-header-nav-menu");
+const device = $(window);
+const topBackButton = $(".js-top-back");
 
-const windowWidth = $(window).width();
+const headerNavMenuToggle = () => {
+  const headerNavMenu = $(".js-header-nav-menu");
+  if (headerNavMenu.hasClass("is-slide-active")) {
+    $(".js-header-nav-menu").on("click", function () {
+      $(".js-header-nav-menu-sub").slideToggle();
+    });
+  }
+};
 
-if (headerNavMenu.hasClass("is-slide-active")) {
-  $(".js-header-nav-menu").on("click", function () {
-    $(".js-header-nav-menu-sub").slideToggle();
+const topBackFunc = () => {
+  topBackButton.on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
   });
-}
+};
+
+device.on("scroll", function () {
+  if (device.scrollTop() >= 80) {
+    topBackButton.fadeIn(300);
+  } else {
+    topBackButton.fadeOut(300);
+  }
+});
+
+headerNavMenuToggle();
+topBackFunc();
