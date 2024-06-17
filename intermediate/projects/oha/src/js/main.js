64ced1641b66fa8e186dfa033642006e14745f38
bunobu <86172592+bunobu@users.noreply.github.com>
2024-06-18 01:28:18 +0900

    :sparkles: swiperのtest #21

--- intermediate/projects/oha/src/js/main.js
+++ intermediate/projects/oha/src/js/main.js
@@ -15,3 +15,24 @@ $(".js-accordion").on("click", function (e) {
     $(this).next().slideDown();
   }
 });
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
