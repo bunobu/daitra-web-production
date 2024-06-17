a8fcb797b82e434bc1dcbf9e94cf0b0fb64d51b7
bunobu <86172592+bunobu@users.noreply.github.com>
2024-06-17 23:02:43 +0900

    :sparkles: qa_アコーディオンの機能実装 #21

--- intermediate/projects/oha/src/js/main.js
+++ intermediate/projects/oha/src/js/main.js
@@ -3,3 +3,15 @@ $("#drawer-icon").on("click", function (e) {
   jQuery("#drawer-icon").toggleClass("drawer-icon--checked");
   jQuery("#drawer-content").toggleClass("drawer-content--checked");
 });
+
+$(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+
+  if ($(this).parent().hasClass("qa-box--open")) {
+    $(this).parent().removeClass("qa-box--open");
+    $(this).next().slideUp();
+  } else {
+    $(this).parent().addClass("qa-box--open");
+    $(this).next().slideDown();
+  }
+});
