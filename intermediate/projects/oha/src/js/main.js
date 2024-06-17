a9560297044a7896a62bfb2b9e816c675090095d
bunobu <86172592+bunobu@users.noreply.github.com>
2024-06-17 20:11:52 +0900

    :sparkles: ドロワーメニューの機能実装 #21

--- intermediate/projects/oha/src/js/main.js
+++ intermediate/projects/oha/src/js/main.js
@@ -1,1 +1,5 @@
-console.log("main");
+$("#drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#drawer-icon").toggleClass("drawer-icon--checked");
+  jQuery("#drawer-content").toggleClass("drawer-content--checked");
+});
