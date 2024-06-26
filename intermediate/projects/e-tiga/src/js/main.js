136523ec7c8a70547fd57028457f61962c9834a2
bunobu <86172592+bunobu@users.noreply.github.com>
2024-06-26 18:57:04 +0900

    :sparkles: ドロワーアニメーションの追加 #27

--- intermediate/projects/e-tiga/src/js/main.js
+++ intermediate/projects/e-tiga/src/js/main.js
@@ -1,8 +1,3 @@
-// import { tamago } from "./tamago";
-
-// console.log("main");
-// tamago();
-
 /**
  * デバイス幅400px以下の場合はviewportを固定
  * flagSize 固定を始めるデバイス幅
@@ -12,13 +7,62 @@
   const viewport = document.querySelector('meta[name="viewport"]');
   function switchViewport() {
     const value =
-      window.screen.width > 428
+      window.screen.width > 400
         ? "width=device-width,initial-scale=1"
-        : "width=428";
+        : "width=400";
     if (viewport.getAttribute("content") !== value) {
       viewport.setAttribute("content", value);
     }
   }
   addEventListener("resize", switchViewport, false);
   switchViewport();
+})();
+
+/**
+ * モーダル表示時の背景固定・解除する
+ */
+let scrollTop = window.scrollY;
+const bodyFixedAdd = () => {
+  scrollTop = window.scrollY;
+
+  document.body.style.cssText = `
+      position: fixed;
+      top: -${scrollTop}px;
+    `;
+
+  console.log("fixed");
+};
+
+const bodyFixedRemove = () => {
+  document.body.style.cssText = `
+      position: "";
+      top: "";
+  `;
+  window.scrollTo({
+    top: scrollTop,
+    left: 0,
+    // behavior: "smooth",
+  });
+};
+
+/**
+ * ドロワーボタンを押したらドロワーを開閉する
+ */
+(() => {
+  const drawerToggle = document.getElementById("js-drawer-toggle");
+  const drawer = document.getElementById("js-drawer");
+
+  if (drawerToggle) {
+    drawerToggle.addEventListener("click", (e) => {
+      e.preventDefault();
+      drawerToggle.classList.toggle("is-active");
+      drawer.classList.toggle("is-active");
+
+      if (drawer.classList.contains("is-active")) {
+        bodyFixedAdd();
+      } else {
+        bodyFixedRemove();
+      }
+    });
+  }
 })();
