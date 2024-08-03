02360532970371554a6048a59737b32298cab5df
bunobu <86172592+bunobu@users.noreply.github.com>
2024-08-03 16:53:39 +0900

    :lipstick: messageとproductのレスポンシブ&ピクパ調整 #29

--- intermediate/projects/sobolon/src/assets/js/main.js
+++ intermediate/projects/sobolon/src/assets/js/main.js
@@ -2,8 +2,6 @@ import { viewportSwitch } from "./viewportSwitch";
 
 viewportSwitch(375);
 
-console.log("main");
-
 const a = document.querySelector("[data-id='hamburger']");
 a.addEventListener("click", () => {
   let active = a.getAttribute("data-open");
@@ -16,16 +14,17 @@ a.addEventListener("click", () => {
 });
 
 // headerの高さ分コンテンツを下げる
+const header = document.getElementById("header");
 const headerFixedSpacer = () => {
-  const header = document.getElementById("header");
   const headerHeight = header.offsetHeight;
   const drawerMenu = document.getElementById("drawerMenu");
-  console.log(headerHeight);
   const main = document.getElementById("headerSpacer");
   main.style.marginBlockStart = `${headerHeight}px`;
   drawerMenu.style.height = `calc(100vh - ${headerHeight}px)`;
 };
-headerFixedSpacer();
-document.addEventListener("resize", () => {
+
+const headerResizeObserver = new ResizeObserver(function () {
   headerFixedSpacer();
+  console.log("headerResizeObserver");
 });
+headerResizeObserver.observe(header);
