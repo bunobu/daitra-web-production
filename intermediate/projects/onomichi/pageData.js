78d3024af134eebfe1af03600c37d1672c7091df
bunobu <86172592+bunobu@users.noreply.github.com>
2024-08-05 20:34:49 +0900

    :lipstick: fvのマークアップ #31

--- intermediate/projects/onomichi/pageData.js
+++ intermediate/projects/onomichi/pageData.js
@@ -1,4 +1,8 @@
-const baseData = {};
+const baseData = {
+  breakPoints: {
+    md: "(min-width: 768px)",
+  },
+};
 
 const pageDate = {
   "/index.html": {
@@ -7,6 +11,7 @@ const pageDate = {
       title: "indexページだよ",
       description: "indexページの説明文だよ",
     },
+    breakPoints: baseData.breakPoints,
   },
   "/about.html": {
     meta: {
